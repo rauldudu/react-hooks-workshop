3a0b57c198deff55fd470f7f19698dec7935528d
Raul Souza <rauleduardo@gmail.com>
2019-10-02 00:12:38 -0700

    Revert "Use reducer hook"
    This reverts commit 318a9aae4e45c3f56a6fbeae686dc10efcbd43e7.

--- src/contexts/Cart.js
+++ src/contexts/Cart.js
@@ -2,31 +2,17 @@ import React from "react";
 
 export const CartContext = React.createContext();
 
-function reducer(state = [], action) {
-  switch (action.type) {
-    case "add":
-      return [...state, action.payload.item];
-    case "delete":
-      return state.filter(item => item.id !== action.payload.id);
-    case "reset":
-      return [];
-    default:
-      throw Error("Invalid action");
-  }
-}
-
 export function CartProvider({ children }) {
-  const [items, dispatch] = React.useReducer(reducer, []);
-
+  const [items, setItems] = React.useState([]);
   const actions = {
     addItem: item => {
-      dispatch({ type: "add", payload: { item } });
+      setItems(prev => [...prev, { ...item }]);
     },
     deleteItem: id => {
-      dispatch({ type: "delete", payload: { id } });
+      setItems(prev => prev.filter(item => item.id !== id));
     },
-    reset: () => {
-      dispatch({ type: "reset" });
+    reset: id => {
+      setItems([]);
     }
   };
 
